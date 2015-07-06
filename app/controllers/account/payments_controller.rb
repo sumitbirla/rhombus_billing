6ba470b8d09c7dc8342275d2347183c4486fe364
Sumit Birla <sbirla@tampahost.net>
2015-07-05 22:31:43 -0400

    account/payments controller

--- app/controllers/account/payments_controller.rb
+++ app/controllers/account/payments_controller.rb
@@ -1,6 +1,11 @@
 class Account::PaymentsController < Account::BaseController
   def index
-    @payments = Payment.where(user_id: session[:user_id]).order(created_at: :desc).page(params[:page])
+    @payments = Payment.where(user_id: session[:user_id], customer: false).order(created_at: :desc).page(params[:page])
+    if params[:t] == "1"
+      @payments = @payments.where("amount > 0.0")
+    elsif params[:t]  == "0"
+      @payments = @payments.where("amount < 0.0")
+    end
   end
 
   def show
