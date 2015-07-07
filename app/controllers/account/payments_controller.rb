0f4179b55a1169e4ca746d2634e35fdc1ff134fe
Sumit Birla <sbirla@tampahost.net>
2015-07-07 22:24:49 +0200

    commission update

--- app/controllers/account/payments_controller.rb
+++ app/controllers/account/payments_controller.rb
@@ -2,9 +2,9 @@ class Account::PaymentsController < Account::BaseController
   def index
     @payments = Payment.where(user_id: session[:user_id], customer: false).order(created_at: :desc).page(params[:page])
     if params[:t] == "1"
-      @payments = @payments.where("amount > 0.0")
-    elsif params[:t]  == "0"
       @payments = @payments.where("amount < 0.0")
+    elsif params[:t]  == "0"
+      @payments = @payments.where("amount > 0.0")
     end
   end
 
