b72d88718e3b5d67748daac8908cda12a62ebcea
Sumit Birla <sbirla@tampahost.net>
2016-10-11 19:19:49 +0200

    updates

--- app/controllers/admin/billing/invoices_controller.rb
+++ app/controllers/admin/billing/invoices_controller.rb
@@ -2,7 +2,7 @@ class Admin::Billing::InvoicesController < Admin::BaseController
   
   def index
     @invoices = Invoice.where(paid: params[:paid])
-                       .order(created_at: :desc)
+                       .order(post_date: :desc)
                        .paginate(page: params[:page], per_page: @per_page)
   end
   
