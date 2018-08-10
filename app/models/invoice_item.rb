f8ee4bafb4d9d4f79bbdfd89b341b864fbfce3f6
Sumit Birla <sbirla@tampahost.net>
2018-08-10 22:55:40 +0200

    add invoice payment type

--- app/models/invoice_item.rb
+++
@@ -1,8 +0,0 @@
-class InvoiceItem < ActiveRecord::Base
-  include Exportable
-  self.table_name = 'bill_invoice_items'
-  
-  belongs_to :invoice
-  validates_presence_of :unit_price, :quantity, :item_number, :item_description
-
-end
