4575633082cd2dc6ab2e04177aadb49cdb15ffcf
Sumit Birla <sbirla@tampahost.net>
2019-04-03 20:25:17 +0200

    updates

--- app/controllers/account/user_packages_controller.rb
+++ app/controllers/account/user_packages_controller.rb
@@ -1,14 +1,16 @@
 class Account::UserPackagesController < Account::BaseController
   
   def index
-    @user_packages = UserPackage.includes(:package).where(user_id: session[:user_id], recurr_status: "A")
+    @user_packages = UserPackage.includes(:package)
+                                .where(user_id: session[:user_id], recurr_status: ["A", "D"])
+                                .where("recurr_date > ?", Date.today)
   end
   
   def destroy
     @user_package = UserPackage.find_by(user_id: session[:user_id], id: params[:id])
-    @user_package.update_column(:recurr_status, "C")
+    @user_package.update_column(:recurr_status, "D")
     
-    flash.now[:success] = "Your subsciption has been cancelled"
+    flash[:success] = "Your subsciption has been marked for cancellation. It will remain active until #{@user_package.recurr_date} "
     redirect_to :back
   end
   
